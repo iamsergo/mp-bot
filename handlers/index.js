cca764d6be9305a7e030c90f9b3af5811310f540
sergey <sergio820015@yandex.ru>
2022-08-25 14:58:14 +0300

    fix: added import from index.js for default handler

--- handlers/index.js
+++ handlers/index.js
@@ -1,7 +1,9 @@
 const MessageHandler = require('./message');
 const StartHandler = require('./start');
+const DefaultHandler = require('./default');
 
 module.exports = {
   MessageHandler,
   StartHandler,
+  DefaultHandler,
 };
