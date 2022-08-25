3c936d6bc58a563ea06a8e16b0eaf7d14310f9a8
sergey <sergio820015@yandex.ru>
2022-08-25 17:46:38 +0300

    fix: removed not needed start for command

--- commands-match/__test__/prediction.test.js
+++ commands-match/__test__/prediction.test.js
@@ -1,11 +1,5 @@
 const { PredictionCommandMatch } = require('../');
 
-
-test('should match for "прогноз "', () => {
-  const text = 'прогноз амкал -2  д 4:sdsadw1';
-  expect(new PredictionCommandMatch(text).isTrue()).toBeTruthy();
-});
-
 test('should match for "прогноз на "', () => {
   const text = 'прогноз на амкas das das dал-2д 4 asdas d:1';
   expect(new PredictionCommandMatch(text).isTrue()).toBeTruthy();
@@ -20,6 +14,7 @@ test('should not match', () => {
   const texts = [
     '/predicitonмкал-2д 4=1',
     'прогнозна амкал-2д 4 asdas d:1',
+    'прогноз амкал-2д 4 asdas d:1',
     'прогнозамкал-2д 4 asdas d:1',
   ];
   texts.forEach(text => {
--- commands-match/prediction.js
+++ commands-match/prediction.js
@@ -4,7 +4,6 @@ class PredictionCommandMatch extends CommandMatch{
   constructor(str) {
     const commandStartsWith = [
       '/prediction ',
-      'прогноз ',
       'прогноз на ',
     ];
     super(str, commandStartsWith);
